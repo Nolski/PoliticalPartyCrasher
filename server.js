a0af5aa988deec7cd6b9648a677ac49ed8d60a15
Mike Nolan <me@michael-nolan.com>
2014-08-08 16:16:30 -0400

    added api for party time

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express  = require('express');
 var config   = require('./server/config');
-var https     = require('https');
+var https    = require('https');
+var http     = require('follow-redirects').http;
 var url      = require('url');
 
 var app = express();
@@ -31,6 +32,33 @@ router.get('/getLegislators', function (req, res) {
     })
     .on('error', function (e) {
         console.log("Got error: " + e.message);
+    });
+});
+
+router.get('/getPartiesForLegislator', function (req, res) {
+    var urlObj = {
+        host: config.partyTimeHost,
+        query: {
+            start_date__gt: req.query.startDate,
+            beneficiaries__crp_id: req.query.crpID,
+            format: 'json',
+            apikey: config.apiKey
+        }
+    }
+    console.log('http:' + url.format(urlObj));
+    http.get('http:' + url.format(urlObj), function (response) {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            res.send(str);
+        });
+    })
+    .on('error', function (e) {
+        console.log("Got error: " + e.message);
+        console.log(e);
     });
 });
 
--- server/config.js
+++ server/config.js
@@ -2,9 +2,6 @@ var API_KEY = 'YOUR_KEY_HERE';
 
 module.exports = {
     apiKey: API_KEY,
-    partyTimeOptions: {
-        host: 'politicalpartytime.org',
-        path: '/api/v1/event/?start_date__gt=2014-07-01&format=json&apikey=' + API_KEY
-    },
+    partyTimeHost: 'politicalpartytime.org/api/v1/event',
     legislatorHost: 'congress.api.sunlightfoundation.com/legislators/locate'
 }
